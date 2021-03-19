f11c28099268b987e4654b3f0cb294886ed88e8c
NexicureAdm <kkt2356@naver.com>
2021-03-19 10:28:36 +0900

    20210319

--- src/Leet40.java
+++ src/Leet40.java
@@ -6,6 +6,24 @@ public class Leet40 {
     }
 
     static List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtracking(list, new ArrayList<>(), candidates, target, 0);
 
+        return list;
+
+    }
+
+    static void backtracking(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList(tempList));
+        else {
+            for(int i= start; i < nums.length; i++) {
+                if (i > start && nums[i] == nums[i-1]) continue;
+                tempList.add(nums[i]);
+                backtracking(list, tempList, nums, remain-nums[i], i+1);
+                tempList.remove(tempList.size()-1);
+            }
+        }
     }
 }
